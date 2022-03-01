54c43f9ecf19eeeb12f73ba930f012473650d169
Ekaterina Kuznetsova <eantonuk@gmail.com>
2022-03-01 14:14:47 +0300

    Added gesture recogniser

--- Controls Practice/ViewController.swift
+++ Controls Practice/ViewController.swift
@@ -10,43 +10,84 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var button: UIButton!
+//
+    
     @IBOutlet var switches: [UISwitch]!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var textField: UITextField!
-    
-    var number: UInt8 = 128 {
+//
+    var number = 128 {
         didSet {
+            if number < 0 {
+                number = 255
+            } else if 255 < number {
+                number = 0
+            }
             updateUI()
         }
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        rotateSwitches()
+        updateUI()
         // Do any additional setup after loading the view.
     }
+    func rotateSwitches() {
+       for `switch` in switches {
+            `switch`.layer.transform = CATransform3DMakeRotation(.pi / 2, 0, 0, 1)
+
+        }
+    }
+        /// Updates number from the switches set
+    func updateNumberFromSwitches() {
+        var number = 0
+        for `switch` in switches {
+            number += `switch`.isOn ? `switch`.tag : 0
+        }
+        self.number = number
+        }
+        /// Updates switсhes from the number
+    func updateSwitches() {
+        for `switch` in switches {
+            `switch`.isOn = Int(number) & `switch`.tag != 0
+            
+        }
+
+       }
+
     /// Updates all outlets to number
     func updateUI() {
         button.setTitle("\(number)", for: [])
-        
-        // toDo: set switches to number
-        
+       updateSwitches()
         slider.value = Float(number)
         textField.text = "\(number)"
-        
+
     }
 
 
     @IBAction func buttonPressed() {
-        number = UInt8((Int(number) + 1) % 256)
+        number += 1
     }
-    @IBAction func switchToggled(_ sender: UISwitch) {
-        print(#line, #function, sender.tag)
-    }
-    @IBAction func sliderMoved() {
-        number = UInt8(slider.value)
-    }
-    @IBAction func textFieldEdited() {
-        number = UInt8(textField.text ?? "") ?? 128
-    }
+
+    
+    @IBAction func switchesToggled(_ sender: UISwitch) {
+        updateNumberFromSwitches()
 }
 
+    @IBAction func sliderMoved() {
+        number = Int(slider.value)
+    }
+    @IBAction func textFieldEdited() {
+        number = Int(textField.text ?? "") ?? 128
+    }
+
+    @IBAction func screenTapped(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: view)
+        if location.x < view.bounds.midX {
+            number -= 1
+        } else {
+            number += 1
+        }
+    }
+}
